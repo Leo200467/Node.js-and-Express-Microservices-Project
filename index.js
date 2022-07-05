683541dd0f17f226abbb65fdf63363f9a93e705a
Leonardo Cardoso <58035475+Leo200467@users.noreply.github.com>
2022-07-04 21:18:58 -0300

    Update package and index to use cors service

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const session = require("express-session");
 
 const redis = require("redis")
 
+const cors = require("cors")
+
 let RedisStore = require("connect-redis")(session)
 
 const { 
@@ -27,10 +29,10 @@ let redisClient = redis.createClient({
     }
 });
 
-redisClient.connect().catch(console.error)
+redisClient.connect().catch(console.error);
 
-const postRouter = require("./routes/postRoutes")
-const userRouter = require("./routes/userRoutes")
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -47,6 +49,10 @@ const connectWithRetry = () => {
 }
 
 connectWithRetry();
+
+app.enable("trust proxy");
+
+app.use(cors({}));
 
 app.use(
     session({
@@ -66,6 +72,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h2> Hi There!! </h2>");
+})
+
+app.get("/api/v1", (req, res) => {
+    console.log("load balanced")
 })
 
 app.use("/api/v1/posts", postRouter)
